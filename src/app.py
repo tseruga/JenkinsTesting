a9d8fe3ce61c6c0113c3f5a01a5e7a8be9faed14
Tyler Seruga <tseruga@umich.edu>
2016-02-10 22:59:17 -0500

    First push

---
+++ src/app.py
@@ -0,0 +1,5 @@
+def addOne(x):
+    return x + 1
+
+def timesTwo(x):
+    return x * 2
---
+++ tests/basic_test.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, r'/home/tyler/Desktop/pytesting/src')
+import app
+
+class TestClass:
+    def test_one(self):
+        x = 3
+        assert app.addOne(x) == 4
+
+    def test_two(self):
+        x = 5
+        assert app.timesTwo(x) == 10
+
+    def test_failing(self):
+        x = 2
+        assert app.timesTwo(x) == 5
